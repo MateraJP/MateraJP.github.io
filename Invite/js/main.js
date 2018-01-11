524bdfad8c0d47d390d0caf6cece32571be94b74
Joao Matera <joao.matera@itixti.com.br>
2018-01-11 18:56:52 -0200

    mais um

--- Invite/js/main.js
+++ Invite/js/main.js
@@ -18,30 +18,6 @@ $(document).ready(function () {
   var getaudio = $('#player')[0];
   var audiostatus = 'off';
 
-  $(document).on('mouseenter', '.speaker', function() {
-    /* Bonus feature, if the mouse hovers over the speaker image for more than 1 second the audio will start playing */
-    if (!mouseovertimer) {
-      mouseovertimer = window.setTimeout(function() {
-        mouseovertimer = null;
-        if (!$('.speaker').hasClass("speakerplay")) {
-          getaudio.load();
-          /* Loads the audio */
-          getaudio.play();
-          /* Play the audio (starting at the beginning of the track) */
-          $('.speaker').addClass('speakerplay');
-          return false;
-        }
-      }, 1000);
-    }
-  });
-
-  $(document).on('mouseleave', '.speaker', function() {
-    /* If the mouse stops hovering on the image (leaves the image) clear the timer, reset back to 0 */
-    if (mouseovertimer) {
-      window.clearTimeout(mouseovertimer);
-      mouseovertimer = null;
-    }
-  });
 
   $(document).on('click touchend', '.speaker', function() {
     /* Touchend is necessary for mobile devices, click alone won't work */
@@ -52,7 +28,6 @@ $(document).ready(function () {
         $('.speaker').addClass("fa-volume-up");
         getaudio.load();
         getaudio.play();
-        window.clearTimeout(mouseovertimer);
         audiostatus = 'on';
         return false;
       } else if (audiostatus == 'on') {
@@ -66,7 +41,6 @@ $(document).ready(function () {
       $('.speaker').removeClass("fa-volume-up");
       $('.speaker').addClass("fa-volume-off");
       $('.speaker').removeClass('speakerplay');
-      window.clearTimeout(mouseovertimer);
       audiostatus = 'on';
     }
   });
